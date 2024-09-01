26a741d06abbdaf7ad38f476d52292c27b57f820
Satyendra Kumar Baghel <146946196+Satyendra0509@users.noreply.github.com>
2024-09-01 15:45:32 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://satyendra-restaurant-app.onrender.com/"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
